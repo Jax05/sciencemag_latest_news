b28e63482b120a0511c4eff1d4a96d9677d36c21
Carley Tripp <carley.tripp@outlook.com>
2017-10-06 03:03:59 +0000

    stubbed out cli.rb

--- lib/sciencemag_latest_news/cli.rb
+++ lib/sciencemag_latest_news/cli.rb
@@ -1,7 +1,34 @@
 class SciencemagLatestNews::CLI
 
   def call
-    puts "Here's the latest news!"
+    greeting
+    list
+    menu
+  end
+
+  def greeting
+    puts "Here's the latest science news!"
+  end
+
+  def list
+    puts "1. Bees"
+    puts "2. Volcano"
+  end
+
+  def menu
+    input = nil
+    # binding.pry
+    while input != "exit"
+      puts "Type the number of the story you'd like to read. You can also type 'list' to see the stories again or 'exit' to exit."
+      input = gets.chomp
+
+      case input
+      when "1" then puts "Here's more info on bees."
+      when "2" then puts "Here's more info on volcano stuff."
+        when "list" then list
+        else "I don't know what you want to do. Type 'list' or 'exit'."
+      end
+    end
   end
 
 end
