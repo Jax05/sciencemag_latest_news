90a0545c189da00bff6c6ed8f9bb6d0eea54118b
Carley Tripp <carley.tripp@outlook.com>
2017-10-06 21:00:28 +0000

    gave story.rb methods basic functionality

--- lib/sciencemag_latest_news/story.rb
+++ lib/sciencemag_latest_news/story.rb
@@ -1,17 +1,22 @@
 class SciencemagLatestNews::Story
+  attr_accessor :headline, :author, :date, :content, :url
 # cli call needs to call a prime method that scrapes all the stories
 # stories need headline, author, date, content, and url
+  @@latest_stories = []
+
   def self.latest_stories
-    # contains latest_stories array
-    # find method can look here
+    @@latest_stories
   end
 
   def self.scrape_latest_stories
     # actually scrapes the site
     # for each listing, create a story instance, gather details & push to latest_stories
+    story = SciencemagLatestNews::Story.new
+    story.headline = "Bees!!"
+    @@latest_stories << story
   end
 
   def self.find(id)
-    # needs to look in stories array
+    @@latest_stories[id - 1]
   end
 end
