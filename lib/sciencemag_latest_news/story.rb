67e1270c6fd617ca50af86b7756ac2342e1a149d
Carley Tripp <carley.tripp@outlook.com>
2017-10-06 20:40:39 +0000

    stubbed out story.rb

--- lib/sciencemag_latest_news/story.rb
+++ lib/sciencemag_latest_news/story.rb
@@ -1,3 +1,17 @@
 class SciencemagLatestNews::Story
+# cli call needs to call a prime method that scrapes all the stories
+# stories need headline, author, date, content, and url
+  def self.latest_stories
+    # contains latest_stories array
+    # find method can look here
+  end
 
+  def self.scrape_latest_stories
+    # actually scrapes the site
+    # for each listing, create a story instance, gather details & push to latest_stories
+  end
+
+  def self.find(id)
+    # needs to look in stories array
+  end
 end
