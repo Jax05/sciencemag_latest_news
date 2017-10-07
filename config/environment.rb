847a272ec67cfa2062e6eaf6e9aa6436e2edc7f2
Carley Tripp <carley.tripp@outlook.com>
2017-10-07 03:24:53 +0000

    created environment file

---
+++ config/environment.rb
@@ -0,0 +1,7 @@
+require "pry"
+require "open-uri"
+require "nokogiri"
+
+require_relative "../lib/sciencemag_latest_news/version"
+require_relative "../lib/sciencemag_latest_news/cli"
+require_relative "../lib/sciencemag_latest_news/story"
--- lib/sciencemag_latest_news.rb
+++ lib/sciencemag_latest_news.rb
@@ -1,11 +1,4 @@
-require "pry"
-require "open-uri"
-require "nokogiri"
-
-require_relative "./sciencemag_latest_news/version"
-require_relative "./sciencemag_latest_news/cli"
-require_relative "./sciencemag_latest_news/story"
-
 module SciencemagLatestNews
-
 end
+
+require_relative '../config/environment'
