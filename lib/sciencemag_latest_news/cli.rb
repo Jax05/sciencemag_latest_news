da27099dc7b1db198cdb2617c171813ce327eb27
Carley Tripp <carley.tripp@outlook.com>
2017-10-07 00:31:00 +0000

    made output more readable and cleaned up interface

--- lib/sciencemag_latest_news/cli.rb
+++ lib/sciencemag_latest_news/cli.rb
@@ -1,21 +1,24 @@
 class SciencemagLatestNews::CLI
 
   def call
-    SciencemagLatestNews::Story.scrape_latest_stories
     greeting
+    SciencemagLatestNews::Story.scrape_latest_stories
     list
     menu
   end
 
   def greeting
     puts "Welcome to ScienceMag Latest News!"
+    puts "Fetching stories..."
   end
 
   def list
+    puts ""
     @stories = SciencemagLatestNews::Story.latest_stories
     @stories.each.with_index(1) do |story, i|
       puts "#{i}. #{story.headline}"
     end
+    puts ""
   end
 
   def menu
@@ -27,15 +30,16 @@ class SciencemagLatestNews::CLI
 
       if input.to_i > 0 && input.to_i <= SciencemagLatestNews::Story.latest_stories.size
         story = SciencemagLatestNews::Story.find(input)
+        puts ""
         puts "#{story.headline}"
         puts "#{story.author} | #{story.date}"
-        puts "#{story.content.join("\n")}"
+        puts ""
+        puts "#{story.content.join("\n\n")}"
+        puts ""
       elsif input == "list"
         list
       elsif input == "exit"
         exit
-      else
-        puts "Please choose one of the options listed."
       end
     end
   end
