390bd88483efca47d70d80ecc0e8d197b9b07a8a
Carley Tripp <carley.tripp@outlook.com>
2017-11-03 22:24:02 +0000

    added colorize dependency

--- config/environment.rb
+++ config/environment.rb
@@ -1,6 +1,7 @@
 require "pry"
 require "open-uri"
 require "nokogiri"
+require "colorize"
 
 require_relative "../lib/sciencemag_latest_news/version"
 require_relative "../lib/sciencemag_latest_news/cli"
